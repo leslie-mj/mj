9176cde96e2651d735baa614421f13e80d68178c
maojun <8905334+maojun_test@user.noreply.gitee.com>
2023-05-11 21:57:32 +0800

    mj

---
+++ pagic.config.ts
@@ -0,0 +1,13 @@
+export default {
+  srcDir: "site",
+  theme: "docs",
+  plugins: ["sidebar", "prev_next", "i18n", "blog", "ga"],
+  title: "MJ",
+  description: "I AM WHAT I AM",
+  nav: [
+    {
+      text: '不知道放啥',
+      link: '../src/doc.md',
+    }
+  ]
+};
